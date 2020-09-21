0b04ca105595e7e7f4ede291bd8815c0dbd1b3f5
hasuke0324 <65121426+hasuke0324@users.noreply.github.com>
2020-09-22 05:16:55 +0900

    Create testdata
    日本語未対応

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Memog;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +14,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // User::factory(10)->create();
+        $this->call([
+            MemogsTableSeeder::class,
+        ]);
     }
 }
